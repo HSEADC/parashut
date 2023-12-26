59bbbc410abd083ae779e4619f1943c02a9484b3
Olga Ostroverkh <ostrova22@mail.ru>
2023-12-26 13:30:10 +0300

    commit all3

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
@@ -15,20 +14,3 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dev_build')
   }
 })
-=======
-const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
-const path = require('path')
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  watch: true,
-  devServer: {
-    static: './dev_build'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dev_build')
-  }
-})
->>>>>>> a7692aba611c3ead47078efb266977b0b269f028
