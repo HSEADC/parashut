752487f6fe87039b34dec6f24fd89ec4de99a0e8
Olga Ostroverkh <ostrova22@mail.ru>
2024-06-26 19:39:39 +0300

    Revert "v2.0 correct old"
    This reverts commit c11021e39dd58b710467b67f7a646d5fdf057e1f.

--- docs/search.ed8ddc595e7d87580792.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-
-function initSearch() {
-  var O_Search = document.querySelector('O_Search');
-}
-/******/ })()
-;
